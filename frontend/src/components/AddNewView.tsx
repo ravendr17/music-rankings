28dbc32f616e6bbdb46fa320db632fdde415e2b8
ravendr17 <ravendr17@gmail.com>
2026-09-01 19:40:56 +0800

    add modal for errors

--- frontend/src/components/AddNewView.tsx
+++ frontend/src/components/AddNewView.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import SongInputRow from "./SongInputRow";
 import {z} from "zod";
 import { reportSchema } from "../schemas";
@@ -38,6 +38,18 @@ export default function AddNewView() {
     {id: 10, title: '', artist: '', playCount: ''}
   ]);
 
+  const [error, setError] = useState('');
+  const [showError, setShowError] = useState(false);
+  const errorRef = useRef<HTMLDialogElement>(null);
+
+  useEffect(() => {
+    if (showError) {
+      errorRef.current?.showModal();
+    } else {
+      errorRef.current?.close();
+    }
+  }, [showError]);
+
   function updateSong(id: number, field: 'title' | 'artist' | 'playCount', value: string) {
     setSongs(songs.map((s) => (
       s.id === id ? {...s, [field]: value}: s
@@ -45,6 +57,9 @@ export default function AddNewView() {
   }
 
   async function handleSubmit() {
+    setError('');
+    setShowError(false);
+
     const filledSongs = songs.filter(s => (
       s.title.trim() || s.artist.trim() || s.playCount.trim()
     ));
@@ -57,7 +72,9 @@ export default function AddNewView() {
     });
 
     if (!parseResult.success) {
-      console.log(z.flattenError(parseResult.error));
+      const errors = (z.flattenError(parseResult.error));
+      setError(JSON.stringify(errors.fieldErrors, null, 2));
+      setShowError(true);
       return;
     }
 
@@ -69,64 +86,87 @@ export default function AddNewView() {
       });
 
       if (!response.ok) {
-        throw new Error('Error');
+        setError(`Failed to submit: ${response.statusText}`);
+        setShowError(true);
+        return;
       }
 
       const result = await response.json();
       console.log(result);
     } catch (error) {
-      console.log(error);
+      setError(error instanceof Error ? error.message: 'Unknown error.');
+      setShowError(true);
     }
   }
 
   return (
-    <div className="flex flex-col">
-      <div className="flex items-center px-4 py-2 gap-10">
-        <input 
-          type="number" 
-          placeholder="Year"
-          className="border-2 rounded-md p-2 bg-white"
-          value={year}
-          onChange={(e) => setYear(e.target.value)}
-        />
-        <select
-          className="border-2 rounded-md px-10 py-2.5 bg-white"
-          value={month}
-          onChange={(e) => setMonth(Number(e.target.value))}
-        >
-          {months.map(v => (
-            <option 
-              value={v.id}
-              key={v.id}
-            >{v.label}
-            </option>
+    <>
+      <div className="flex flex-col">
+        <div className="flex items-center px-4 py-2 gap-10">
+          <input 
+            type="number" 
+            placeholder="Year"
+            className="border-2 rounded-md p-2 bg-white"
+            value={year}
+            onChange={(e) => setYear(e.target.value)}
+          />
+          <select
+            className="border-2 rounded-md px-10 py-2.5 bg-white"
+            value={month}
+            onChange={(e) => setMonth(Number(e.target.value))}
+          >
+            {months.map(v => (
+              <option 
+                value={v.id}
+                key={v.id}
+              >{v.label}
+              </option>
+            ))}
+          </select>
+          <input 
+            type="number" 
+            placeholder="Total Hours"
+            className="border-2 rounded-md p-2 bg-white"
+            value={totalHours}
+            onChange={(e) => setTotalHours(e.target.value)}
+          />
+        </div>
+        <div className="grid grid-cols-[1fr_6fr_6fr_1fr] gap-2 items-center px-4 py-2">
+          <span className="font-bold text-center text-lg">Rank</span>
+          <span className="font-bold text-lg">Song Title</span>
+          <span className="font-bold text-lg">Song Artist</span>
+          <span className="font-bold text-lg">Play Count</span>
+          {songs.map((song) => (
+            <SongInputRow key={song.id} song={song} updateSong={updateSong} />
           ))}
-        </select>
-        <input 
-          type="number" 
-          placeholder="Total Hours"
-          className="border-2 rounded-md p-2 bg-white"
-          value={totalHours}
-          onChange={(e) => setTotalHours(e.target.value)}
-        />
+        </div>
+        <div className="flex justify-end px-4 py-2">
+          <button 
+            className="border rounded-md px-12 py-2.5 bg-green-600 text-white font-bold text-lg
+              hover:bg-green-500 cursor-pointer"
+            onClick={() => handleSubmit()}
+          >Submit
+          </button>
+        </div>
       </div>
-      <div className="grid grid-cols-[1fr_6fr_6fr_1fr] gap-2 items-center px-4 py-2">
-        <span className="font-bold text-center text-lg">Rank</span>
-        <span className="font-bold text-lg">Song Title</span>
-        <span className="font-bold text-lg">Song Artist</span>
-        <span className="font-bold text-lg">Play Count</span>
-        {songs.map((song) => (
-          <SongInputRow key={song.id} song={song} updateSong={updateSong} />
-        ))}
-      </div>
-      <div className="flex justify-end px-4 py-2">
-        <button 
-          className="border rounded-md px-12 py-2.5 bg-green-600 text-white font-bold text-lg
-            hover:bg-green-500 cursor-pointer"
-          onClick={() => handleSubmit()}
-        >Submit
-        </button>
-      </div>
-    </div>
+      
+      <dialog 
+        ref={errorRef} 
+        className="m-auto rounded-md bg-white backdrop:bg-black/50"
+      >
+        <div className="flex flex-col p-6 gap-4">
+          <span className="text-lg font-bold text-red-600 flex justify-center">Errors</span>
+          <pre className="text-sm">{error}</pre>
+          <div className="flex justify-end">
+            <button 
+              onClick={() => setShowError(false)}
+              className="bg-red-600 text-white px-8 py-2 rounded"
+            >
+              Close
+            </button>
+          </div>
+        </div>
+      </dialog>
+    </>
   );
 }
