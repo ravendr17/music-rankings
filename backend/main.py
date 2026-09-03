5947d9e452a524897de5597ddc9f71f2c13e3dec
ravendr17 <ravendr17@gmail.com>
2026-09-03 22:32:07 +0800

    add CORS middleware and initial create_report endpoint

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,25 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from schemas import ReportCreate
 
 app = FastAPI()
+
+origins = [
+    'http://localhost:5173'
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*']
+)
 
 @app.get('/')
 def home():
     return {'message': 'hello world'}
+
+@app.post('/api/reports')
+def create_report(report: ReportCreate):
+    return report
